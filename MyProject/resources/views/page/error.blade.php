aad6edd74e62c5c4d778d1af08ecd7b657dfc4b5
Ngô Anh Tuấn <19220963@st.neu.edu.vn>
2023-06-21 21:58:51 +0700

    Add Middleware UserCheck

---
+++ MyProject/resources/views/page/error.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+@section('title', 'Error')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center py-5">Error</h1>
+            </div>
+        </div>
+    </div>
+@endsection
