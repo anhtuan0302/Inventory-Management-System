c23f3cc5b54451f20fb1675303bf5ab40b3ebba6
Ngô Anh Tuấn <19220963@st.neu.edu.vn>
2023-06-21 21:59:06 +0700

    Update Routes

--- MyProject/routes/web.php
+++ MyProject/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SalaryController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\WarehouseController;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +21,29 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::resource('/products', ProductController::class);
+Route::resource('/categories', CategoryController::class);
+Route::resource('/suppliers', SupplierController::class);
+Route::resource('/employees', EmployeeController::class);
+Route::resource('/warehouses', WarehouseController::class);
+Route::resource('/salaries', SalaryController::class);
+Route::resource('/users', UserController::class);
+Route::get('/login', AuthenticationController::class . '@loginIndex')->name('login');
+Route::get('/register', AuthenticationController::class . '@registerIndex');
+Route::post('/login', AuthenticationController::class . '@login');
+Route::post('/register', AuthenticationController::class . '@register');
+Route::get('/logout', AuthenticationController::class . '@logout');
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.dashboard');
+})->middleware('UserCheck:User');
+Route::get('error', function () {
+    return view('page.error');
 });
+
+//Middlaware
+Route::get('/suppliers/create', [SupplierController::class, 'create'])->middleware('UserCheck:User');
+Route::get('/suppliers/edit', [SupplierController::class, 'edit'])->middleware('UserCheck:User');
+Route::get('/suppliers/delete', [SupplierController::class, 'delete'])->middleware('UserCheck:User');
+Route::get('/users', [UserController::class, 'index'])->middleware('UserCheck:User');
+Route::get('/users/create', [UserController::class, 'create'])->middleware('UserCheck:User');
+Route::get('/users/delete', [UserController::class, 'delete'])->middleware('UserCheck:User');
